7f8f111dc671d527275482e7f593cbf3629f6b01
zeon0xx0 <jihyon5615@naver.com>
2026-04-24 13:45:50 +0900

    refactor: 전역 스타일 수정사항 반영

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import { AppStyleProvider } from './styles/AppStyleProvider';
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <AppStyleProvider>
-//       <App />
-//     </AppStyleProvider>
-//   </React.StrictMode>,
-// );
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
