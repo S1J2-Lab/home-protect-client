b7760f0dde3ea6458ed1922b162a33a73e558a28
2seb2 <yuo231@naver.com>
2026-05-05 01:24:07 +0900

    chore: AppRouter에 LandingPage, AnalysisLoadingPage 라우트 추가 (#36)
    - / 경로에 LandingPage 연결
    - /analyze 경로에 AnalysisLoadingPage 연결

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { InputPage } from '../pages/InputPage/InputPage';
+import { LandingPage } from '../pages/LandingPage/LandingPage';
+import { AnalysisLoadingPage } from '../pages/AnalyzingPage/AnalysisLoadingPage';
 
 export function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<LandingPage />} />
       <Route path="/input" element={<InputPage />} />
+      <Route path="/analyze" element={<AnalysisLoadingPage />} />
     </Routes>
   );
 }
