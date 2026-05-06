c3f15a8772b7a7d4b9fc98c44a73e230e6e1d918
zeon0xx0 <jihyon5615@naver.com>
2026-05-06 22:56:12 +0900

    refactor: base_url 파일 분리 및 address 파일 백엔드 api 응답에 맞게 코드 수정

--- src/api/address.ts
+++ src/api/address.ts
@@ -1,14 +1,22 @@
 import axios from 'axios';
+import { BASE_URL } from './config';
 import type { Address } from '../types/address';
 
-const BASE_URL = 'http://52.79.87.217:8080';
+interface AddressResponse {
+  data: {
+    results: Address[];
+  };
+}
 
 export async function searchAddress(query: string): Promise<Address[]> {
-  const { data } = await axios.get<Address[]>(`${BASE_URL}/address/search`, {
-    params: {
-      query,
+  const { data } = await axios.get<AddressResponse>(
+    `${BASE_URL}/address/search`,
+    {
+      params: {
+        query,
+      },
     },
-  });
+  );
 
-  return data;
+  return data.data.results;
 }
---
+++ src/api/config.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = 'http://52.79.87.217:8080';
